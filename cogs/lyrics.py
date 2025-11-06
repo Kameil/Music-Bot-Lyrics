66da36cdceb801082ed9bdd6596f2e011122f8f3
cafewhaze <contaparaaescola60@gmail.com>
2025-11-06 11:46:10 -0300

    Tava duplicado alguns input

--- cogs/lyrics.py
+++ cogs/lyrics.py
@@ -1,13 +1,8 @@
-from discord.ext import commands
-from discord import app_commands
 import httpx        
 import discord
 
 from discord.ext import commands
 from discord import app_commands
-import httpx
-import discord
-
 
 class Lyrics(commands.Cog):
     def __init__(self, bot: commands.Bot):
