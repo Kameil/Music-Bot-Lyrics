ff1b42f84ac824007af22845491748c5f1c879c3
sankoofa <83366426+sankoofa@users.noreply.github.com>
2026-01-09 13:53:14 -0300

    Create config.example.py

---
+++ config.example.py
@@ -0,0 +1,7 @@
+token = "your_token_here"
+last_fm_application_name = "your_application_name"
+last_fm_API_key = "your_API_key_here"
+last_fm_shared_secret = "your_fm_shared_secret" 
+Registered_to = "your_name"
+
+# DELETE THIS FILE AND CREATE ONE WITH THE INFORMATION ABOVE CALLED ONLY config.py !!!
